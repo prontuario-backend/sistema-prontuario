5477144463a5c6b1b31a6370c4b73a46d7be1930
prontuario-backend <161663919+prontuario-backend@users.noreply.github.com>
2024-02-28 22:12:00 -0300

    Teste
    um teste para saber se o server do github executa arquivos php.

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        h1{
+            color:blue;
+        }
+    </style>
+</head>
+<body>
+    <?php 
+    
+    echo "<h1>Deu certo edu</h1>";
+    
+    ?>
+</body>
+</html>
