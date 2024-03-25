2ffdecc76f4294b34cf08fb8fcd3aa91af073b1e
prontuario <prontuariobackend@gmail.com>
2024-03-25 10:10:15 -0300

    dfgh

--- frontend/paginas/balancohidrico.php
+++ frontend/paginas/balancohidrico.php
@@ -2,5 +2,19 @@
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'interFront.php';
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'balancohidrico.html';
 
-$inter = new InterBalHidrico();
-$inter->create()
+// $data = $_POST['m-data'];
+// $data = $_POST['m-hora'];
+// $data = $_POST['m-lqdA'];
+// $data = $_POST['m-lqdE'];
+// $data = $_POST['m-conc'];
+// $data = $_POST['m-voral'];
+// $data = $_POST['m-vpare'];
+// $data = $_POST['m-qAdm'];
+// $data = $_POST[''];
+// $data = $_POST[''];
+// $data = $_POST[''];
+// $data = $_POST[''];
+// $data = $_POST[''];
+// $data = $_POST[''];
+
+// $inter = new InterBalHidrico();
